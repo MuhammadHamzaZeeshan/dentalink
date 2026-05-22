0cb729dac8839d12d49e439aee86d50f6ca20493
Muhammad Hamza Zeeshan <hamzazeeshan675@gmail.com>
2026-05-22 15:56:27 +0500

    add API views

--- backend/dental_supply/views.py
+++ backend/dental_supply/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import DentalProduct, ProcurementOrder, VendorStockListing
+from .serializers import ProcurementOrderSerializer, DentalProductSerializer, VendorStockListingSerializer
 
-# Create your views here.
+class ProductListAPIView(APIView):
+     def get(self, request):
+        items = DentalProduct.objects.all()
+        serializer = DentalProductSerializer(items, many=True)
+        return Response(serializer.data)
+     
+class PriceListAPIView(APIView):
+    def get(self, request):
+        items = VendorStockListing.objects.filter(is_available=True)
+        serializer = VendorStockListingSerializer(items, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = VendorStockListingSerializer(request)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class OrderListAPIView(APIView):
+    def get(self, request):
+        items = ProcurementOrder.objects.all()
+        serializer = ProcurementOrderSerializer(items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProcurementOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
