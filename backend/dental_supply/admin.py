0ecfdff9ecb2316cb47edd44218b2973f50b2f9d
Muhammad Hamza Zeeshan <hamzazeeshan675@gmail.com>
2026-05-22 12:05:55 +0500

    reg models

--- backend/dental_supply/admin.py
+++ backend/dental_supply/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.CustomUser)
+admin.site.register(models.DentalProduct)
+admin.site.register(models.VendorStockListing)
+admin.site.register(models.ProcurementOrder)
